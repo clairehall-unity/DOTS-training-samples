eba29253b769cda06f088b2360d89c8f9cf72884
Claire Hall <claire.hall@unity3d.com>
2020-02-18 11:21:50 +0000

    Tidy up bee manager

--- Ported/DOTS - Combat Bees/Assets/Scripts/BeeManagerSystem.cs
+++ Ported/DOTS - Combat Bees/Assets/Scripts/BeeManagerSystem.cs
@@ -21,43 +21,37 @@ public class BeeManagerSystem : JobComponentSystem
     }
     
     EntityCommandBufferSystem EndInitCommandBufferSystem;
-
-    EntityQuery SpawnBees;
+    
     EntityQuery BeeManager;
 
     protected override void OnCreate()
     {
         EndInitCommandBufferSystem = World.GetExistingSystem<EndInitializationEntityCommandBufferSystem>();
         
-        SpawnBees = GetEntityQuery(ComponentType.ReadOnly<BeeManagerData>(), ComponentType.ReadOnly<SpawnBeeData>());
         BeeManager = GetEntityQuery(ComponentType.ReadOnly<BeeManagerData>());
     }
 
     protected override JobHandle OnUpdate(JobHandle inputDependencies)
     {
-        //var managerData = BeeManager.GetSingleton<BeeManagerData>();
-        
-        //var spawnJobHandle = new SpawnBeesJob{ CommandBuffer = EndInitCommandBufferSystem.CreateCommandBuffer().ToConcurrent()}.Schedule(SpawnBees, inputDependencies);
-        //EndInitCommandBufferSystem.AddJobHandleForProducer(spawnJobHandle);
-
+        var managerData = BeeManager.GetSingleton<BeeManagerData>();
         var commandBuffer = EndInitCommandBufferSystem.CreateCommandBuffer();
 
-        Entities.ForEach((Entity entity, RenderMeshInfo meshInfo, ref BeeManagerData beeManagerData, ref SpawnBeeData spawnBeeData) =>
+        Entities.ForEach((Entity entity, RenderMeshInfo meshInfo, ref SpawnBeeData spawnBeeData) =>
             {
                 for (int i = 0; i < spawnBeeData.SpawnCount; i++)
                 {
                     var teamIndex = i % 2; 
                     var beeEntity = commandBuffer.CreateEntity();
                     
-                    var position = Vector3.right * ((-beeManagerData.FieldSize.x * 0.4f) + beeManagerData.FieldSize.x * 0.8f * teamIndex);
+                    var position = Vector3.right * ((-managerData.FieldSize.x * 0.4f) + managerData.FieldSize.x * 0.8f * teamIndex);
                     position.y += i;
                     
                     commandBuffer.AddComponent(beeEntity, new Translation { Value = position });
-                    commandBuffer.AddComponent(beeEntity, new NonUniformScale{ Value = new float3(beeManagerData.MinBeeSize,  beeManagerData.MinBeeSize, beeManagerData.MinBeeSize) });
+                    commandBuffer.AddComponent(beeEntity, new NonUniformScale{ Value = new float3(managerData.MinBeeSize,  managerData.MinBeeSize, managerData.MinBeeSize) });
                     commandBuffer.AddComponent(beeEntity, new Bee{ Velocity = new float3(), TeamIndex = teamIndex});
                     commandBuffer.AddSharedComponent(beeEntity, new RenderMeshWithColourInfo
                     {
-                        Colour = teamIndex == 0 ? beeManagerData.TeamAColour : beeManagerData.TeamBColour,
+                        Colour = teamIndex == 0 ? managerData.TeamAColour : managerData.TeamBColour,
                         Mesh = meshInfo.Mesh,
                         Material = meshInfo.Material
                     });
